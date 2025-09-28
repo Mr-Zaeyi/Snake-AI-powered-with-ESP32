5d431995414109c309fad47976100a4d6efc9d39
Nohé Régis <regis.nohe@iut-tlse3.fr>
2025-09-28 15:13:17 +0200

    test message Boot sur UART2, ajout MacroConstante dans platformio.ini pour clignotement LED Verte

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
 #include <Arduino.h>
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   Serial.println("Boot");
+
+  pinMode(LED_VERTE, OUTPUT);
 }
 
-void loop() {
- 
+void loop()
+{
+  digitalToggle(LED_VERTE);
+  delay(1000);
 }
